0824247f95794403fd107e144333568d821454b2
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-16 06:11:15 +0900

    ㅓㅗ

--- src/test/test_JH_eclipse2.java
+++ src/test/test_JH_eclipse2.java
@@ -4,5 +4,8 @@ public class test_JH_eclipse2 {
 	public static void main(String[] args) {
         System.out.println("이클립스에서 하는 커밋 테스트");
         System.out.println("성공");
+        
+        System.out.println("이클립스에서 하는 커밋 테스트2");
+        System.out.println("도전");
 	}
 }
