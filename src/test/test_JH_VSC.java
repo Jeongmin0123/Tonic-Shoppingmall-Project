12a0c6ede50f133913c040b50c8b37e03df65d2b
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-15 22:07:22 +0900

    UPDATE

--- src/test/test_JH_VSC.java
+++ src/test/test_JH_VSC.java
@@ -2,6 +2,7 @@ package test;
 
 public class test_JH_VSC {
     public static void main(String[] args) {
-        System.out.println("VSC 커밋 테스트");
+        System.out.println("비쥬얼스튜디오코드에서 하는 커밋 테스트");
+        System.out.println("성공");
     }
 }
