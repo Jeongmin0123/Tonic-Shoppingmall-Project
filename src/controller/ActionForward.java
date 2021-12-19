548d1f576e50925709a4a685461326233a575922
Jeongmin Jang <xvsgxv@gmail.com>
2021-12-20 04:02:39 +0900

    member, employee insert controller 작성, controller 기본 및 EncFilter 작성

---
+++ src/controller/ActionForward.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class ActionForward {
+	private boolean redirect;	
+	private String path;		
+	
+	// redirect방식 T : 전달할 정보가 없음
+	// forward 방식 F : 전달할 정보가 있음
+	
+	public boolean isRedirect() {
+		return redirect;
+	}
+	public void setRedirect(boolean redirect) {
+		this.redirect = redirect;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
---
+++ src/controller/NoticeAction.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.notice.NoticeDAO;
+import model.notice.NoticeVO;
+
+public class NoticeAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		NoticeDAO dao = new NoticeDAO();
+		int count = Integer.parseInt(request.getParameter("count"));
+		ArrayList<NoticeVO> datas=dao.selectAll(count);	// noticeSet의 존재 이유???
+		request.setAttribute("datas", datas);
+		return null;
+		
+		ActionForward forward = new ActionForward();
+		forward.setPath("notice.jsp");	// 어디로 가야하는지 : 경로
+		forward.setRedirect(false);		// 어떻게 가야하는지 : 방식
+		return forward;
+	}
+
+}
