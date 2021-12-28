6b969ba91917ca778d12926a8f3e9057410f37e2
Jeongmin Jang <xvsgxv@gmail.com>
2021-12-28 14:58:40 +0900

    수정중

--- src/controller/notice/NoticeFrontController.java
+++ src/controller/notice/NoticeFrontController.java
@@ -50,31 +50,31 @@ public class NoticeFrontController extends HttpServlet {
 		System.out.println(command);
 		
 		ActionForward forward=null;
-		if(command.equals("notice.not")) {		// main 페이지의 공지사항 부분 클릭 시 notice.not를 통해 notice.jsp로 이동
+		if(command.equals("/notice.not")) {		// main 페이지의 공지사항 부분 클릭 시 notice.not를 통해 notice.jsp로 이동
 			try {
 				forward= new NoticeAction().execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(command.equals("notice_detail.not")) { // notice.jsp 페이지에서 공지사항 중 하나를 클릭시 notice_detail.not를 통해 noticedetail.jsp 페이지로 이동
+		} else if(command.equals("/notice_detail.not")) { // notice.jsp 페이지에서 공지사항 중 하나를 클릭시 notice_detail.not를 통해 noticedetail.jsp 페이지로 이동
 			try {
 				forward= new NoticeDetailAction().execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(command.equals("notice_insert.not")) { // 공지사항 작성 버튼 클릭 시 작성 후에 notice.not 을 통해 notice.jsp 로 이동
+		} else if(command.equals("/notice_insert.not")) { // 공지사항 작성 버튼 클릭 시 작성 후에 notice.not 을 통해 notice.jsp 로 이동
 			try {
 				forward= new NoticeWriteAction().execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(command.equals("notice_update.not")) { // 공지사항 수정 버튼 클릭 시 수정 후에 notice.not 을 통해 notice.jsp 로 이동
+		} else if(command.equals("/notice_update.not")) { // 공지사항 수정 버튼 클릭 시 수정 후에 notice.not 을 통해 notice.jsp 로 이동
 			try {
 				forward= new NoticeUpdateAction().execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(command.equals("notice_delete.not")) { // 공지사항 삭제 버튼 클릭 시 삭제 후에 notice.not 을 통해 notice.jsp 로 이동
+		} else if(command.equals("/notice_delete.not")) { // 공지사항 삭제 버튼 클릭 시 삭제 후에 notice.not 을 통해 notice.jsp 로 이동
 			try {
 				forward= new NoticeDeleteAction().execute(request, response);
 			} catch (Exception e) {
