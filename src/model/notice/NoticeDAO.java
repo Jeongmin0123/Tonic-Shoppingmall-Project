5d7710e30021c097c9ed1b13cbd100b4a7f24c97
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-31 12:45:34 +0900

    NoticeDAO 검색 메서드

--- src/model/notice/NoticeDAO.java
+++ src/model/notice/NoticeDAO.java
@@ -23,6 +23,8 @@ public class NoticeDAO {
 	private String sql_selectOne = "SELECT * FROM notice WHERE nidx=?";
 	private String sql_updateN = "UPDATE notice SET ntitle=?, ncont=? WHERE nidx=?";
 	private String sql_deleteN = "DELETE FROM notice WHERE nidx = ?"; 
+	private String sql_searchN = "SELECT * FROM board WHERE ntitle LIKE '%'||?||'%'"; // 제목 기반 검색 쿼리
+	
 	
 	public boolean insertNotice(NoticeVO vo) {
 		int result = 0;
@@ -44,9 +46,9 @@ public class NoticeDAO {
 		return result == 1;
 	}
 
-//  공지사항 가져오기 메서드
-//  String sql_selectAll = "SELECT * FROM notice";
-//  수정내용: 뷰단에서 구분하기 쉽도록 nlist, notice 변수명 변경, 인자값 삭제
+	// 공지사항 가져오기 메서드
+	// sql_selectAll = "SELECT * FROM notice";
+	// 수정내용: 뷰단에서 구분하기 쉽도록 nlist, notice 변수명 변경, 인자값 삭제
 	public ArrayList<NoticeVO> selectAll() {
 		ArrayList<NoticeVO> nlist = new ArrayList<NoticeVO>();
 		NoticeVO notice = null;
@@ -73,8 +75,8 @@ public class NoticeDAO {
 		return nlist.isEmpty()? null : nlist;
 	}
 	
-//	공지사항 본문, 필요한 메서드인 걸 어필한다!
-//  String sql_selectOne = "SELECT * FROM notice WHERE nidx=?";
+	// 공지사항 본문, 필요한 메서드인 걸 어필한다!
+	// sql_selectOne = "SELECT * FROM notice WHERE nidx=?";
 	public NoticeVO selectOne(NoticeVO vo) {
 		NoticeVO notice = null;
 		
@@ -99,8 +101,8 @@ public class NoticeDAO {
 		return notice;
 	}
 	
-//  공지사항 수정
-//  String sql_updateN = "UPDATE notice SET ntitle=?, ncont=? WHERE nidx=?";
+	// 공지사항 수정
+	// sql_updateN = "UPDATE notice SET ntitle=?, ncont=? WHERE nidx=?";
 	public boolean updateNotice(NoticeVO vo) {
 		int result = 0;
 		
@@ -120,8 +122,8 @@ public class NoticeDAO {
 		return result == 1;
 	}
 	
-//  관리자인지 확인하고 삭제를 허용한다. 수정 중	
-//	String sql_deleteN = "DELETE FROM notice WHERE nidx = ?"; 
+	// 관리자인지 확인하고 삭제를 허용한다. 수정 중	
+	// sql_deleteN = "DELETE FROM notice WHERE nidx = ?"; 
 	public boolean deleteNotice(NoticeVO vo) {
 		int result = 0;
 		
@@ -137,5 +139,36 @@ public class NoticeDAO {
 			JDBCUtil.disconnect(rs, pstmt, con);
 		}
 		return result == 1;
+	}
+	
+	// 검색 메서드, SQL문 작성
+	// sql_searchN = "SELECT * FROM board WHERE ntitle LIKE '%' || ? || '%'";
+	public ArrayList<NoticeVO> searchNotice(String searchTitle) { // 인자로 뭘 받을 것인가
+		ArrayList<NoticeVO> nlist = new ArrayList<>();
+		NoticeVO notice = null;
+		
+		con = JDBCUtil.connect();
+		try {
+			pstmt = con.prepareStatement(sql_searchN);
+			if(searchTitle != null && !searchTitle.equals("") && !searchTitle.equals(" ")) {
+				pstmt.setString(1, searchTitle.trim());
+			}
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				notice = new NoticeVO();
+				notice.setNidx(rs.getInt("nidx"));
+				notice.setNtitle(rs.getString("ntitle"));
+				notice.setNcont(rs.getString("ncont"));
+				notice.setWriter(rs.getString("writer"));
+				
+				nlist.add(notice);
+			}
+		} catch(SQLException e) {
+			System.out.println("NoticeDAO searchNotice(): "+ e +" 에러");
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.disconnect(rs, pstmt, con);
+		}
+		return nlist.isEmpty()? null : nlist;
 	}
 }
