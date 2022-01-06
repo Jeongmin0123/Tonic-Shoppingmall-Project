bdf2f90e091c5ba314c5617a66c8479c163a0a90
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2022-01-06 20:27:03 +0900

    회원정보 성별, 연락처 회원수정 추가

--- src/model/member/MemberDAO.java
+++ src/model/member/MemberDAO.java
@@ -26,7 +26,7 @@ public class MemberDAO {
 	private String sql_loginM = "SELECT * FROM member WHERE mid=?"; 
 	private String sql_selectM = "SELECT * FROM member WHERE mid=?"; 
 	private String sql_findIDbyTel = "SELECT mid FROM member WHERE mtel=?"; 
-//	private String sql_updateM = "UPDATE member SET mname=?, mbirth=?,"
+//	private String sql_updateM1 = "UPDATE member SET mname=?, mbirth=?,"
 //			+ "maddr_zipcode=?, maddr_street=?, maddr_detail=?, maddr_etc=?,"
 //			+ "memail=? WHERE mid=?"; 
 //	private String sql_updateM2 = "UPDATE member a SET mname=?, mbirth=?,"
@@ -35,10 +35,10 @@ public class MemberDAO {
 //	private String sql_updateM3 = "UPDATE member a SET mname=?, mbirth=?,"
 //			+ "maddr_zipcode=?, maddr_street=?, maddr_detail=?, maddr_etc=?,"
 //			+ "memail=? WHERE EXISTS (SELECT * FROM member WHERE mid = ?"; 
-	private String sql_updateM4 = "UPDATE member SET mname=nvl(?, mname), mbirth = nvl(?, mbirth), "
-			+ "maddr_zipcode=nvl(?, maddr_zipcode), maddr_street=nvl(?, maddr_street) , maddr_detail=nvl(?, maddr_detail), "
-			+ "maddr_etc=nvl(?, maddr_etc), "
-			+ "memail=nvl(?, memail) WHERE mid = ?"; 
+	private String sql_updateM = "UPDATE member SET mname=nvl(?, mname), mgender=nvl(?, mgender), "
+			+ "mbirth=nvl(?, mbirth), maddr_zipcode=nvl(?, maddr_zipcode), maddr_street=nvl(?, maddr_street), "
+			+ "maddr_detail=nvl(?, maddr_detail), maddr_etc=nvl(?, maddr_etc), mtel = nvl(?, mtel), "
+			+ "memail=nvl(?, memail) WHERE mid = ?";
 	private String sql_getMemberList = "SELECT * FROM member"; 
 	private String sql_isExistID = "SELECT * FROM member WHERE mid=?";
 	private String sql_selectPW = "SELECT mpw FROM member WHERE mid=?";      
@@ -161,17 +161,17 @@ public class MemberDAO {
 		
 		con = JDBCUtil.connect();
 		try {
-			pstmt = con.prepareStatement(sql_updateM4);
+			pstmt = con.prepareStatement(sql_updateM);
 			pstmt.setString(1, vo.getMname());
-            pstmt.setString(2, vo.getMbirth());
-//          pstmt.setString(3, member.getMaddr_zipcode()+" "
-//          		+member.getMaddr_street()+" "+member.getMaddr_detail());
-            pstmt.setString(3, vo.getMaddr_zipcode());
-            pstmt.setString(4, vo.getMaddr_street());
-            pstmt.setString(5, vo.getMaddr_detail());
-            pstmt.setString(6, vo.getMaddr_etc());
-            pstmt.setString(7, vo.getMemail());
-            pstmt.setString(8, vo.getMid());
+			pstmt.setString(2, vo.getMgender());
+            pstmt.setString(3, vo.getMbirth());
+            pstmt.setString(4, vo.getMaddr_zipcode());
+            pstmt.setString(5, vo.getMaddr_street());
+            pstmt.setString(6, vo.getMaddr_detail());
+            pstmt.setString(7, vo.getMaddr_etc());
+            pstmt.setString(8, vo.getMtel());
+            pstmt.setString(9, vo.getMemail());
+            pstmt.setString(10, vo.getMid());
             result = pstmt.executeUpdate(); 
 		} catch(Exception e) {
 			System.out.println("MemberDAO updateMember(): "+ e +" 에러");
