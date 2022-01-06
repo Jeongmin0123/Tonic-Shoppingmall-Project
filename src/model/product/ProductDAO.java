8e52fa2637303cee9c3ea8c45f2a26fa0acbb90e
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2022-01-06 20:04:37 +0900

    에러문 수정

--- src/model/product/ProductDAO.java
+++ src/model/product/ProductDAO.java
@@ -181,7 +181,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAll() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllTOP() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -256,7 +256,7 @@ public class ProductDAO {
 	}
 
 	// 매진임박순
-	public ArrayList<ProductVO> sql_selectAlmostSoldOut() {
+	public ArrayList<ProductVO> selectAlmostSoldOut() {
 		ArrayList<ProductVO> plist = new ArrayList<>();
 		
 		con = JDBCUtil.connect();
@@ -281,7 +281,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAllName() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAlmostSoldOut() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -315,7 +315,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAll() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllTOP6() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -348,7 +348,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAllHigh() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllHigh6() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -381,7 +381,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAllLow() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllLow6() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -389,7 +389,7 @@ public class ProductDAO {
 		return plist.isEmpty()? null : plist;
 	}
 	// 매진임박순 TOP6
-	public ArrayList<ProductVO> sql_selectAlmostSoldOut6() {
+	public ArrayList<ProductVO> selectAlmostSoldOut6() {
 		ArrayList<ProductVO> plist = new ArrayList<>();
 		
 		con = JDBCUtil.connect();
@@ -414,7 +414,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectAllName() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAlmostSoldOut6() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -448,7 +448,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectOne() : "+ e +" 에러");
+			System.out.println("ProductDAO selectTOPPcode() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -482,7 +482,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectOne() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllHPPcode() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -516,7 +516,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectOne() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAllLPPcode() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
@@ -550,7 +550,7 @@ public class ProductDAO {
 				plist.add(product);
 			}
 		} catch(Exception e) {
-			System.out.println("ProductDAO selectOne() : "+ e +" 에러");
+			System.out.println("ProductDAO selectAlmostSoldOutPcode() : "+ e +" 에러");
 			e.printStackTrace();
 		} finally {
 			JDBCUtil.disconnect(rs, pstmt, con);
